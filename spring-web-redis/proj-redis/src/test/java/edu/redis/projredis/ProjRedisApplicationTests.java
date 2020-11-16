b99997f7ee3c70b6c032bb9998d80211bb87368b
Dipayan-Das1 <dipayan1.das@gmai.com>
2020-11-16 07:17:48 +0530

    Application test class

---
+++ spring-web-redis/proj-redis/src/test/java/edu/redis/projredis/ProjRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.redis.projredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
